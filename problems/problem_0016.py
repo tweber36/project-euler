cc4f6b981ca7897ab05c2b52461b189394839786
Thomas Weber <tweber36@gmail.com>
2019-06-08 11:17:09 +0200

    P30

--- problems/problem_0016.py
+++ problems/problem_0016.py
@@ -4,8 +4,8 @@ from pyler import EulerProblem
 
 class Problem0016(EulerProblem, unittest.TestCase):
     """
-    215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26. What is the
-    sum of the digits of the number 21000?
+    2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26. What is the
+    sum of the digits of the number 2^1000?
     """
 
     problem_id = 16
