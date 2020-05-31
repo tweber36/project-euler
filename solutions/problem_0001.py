14c95469f21d0b0e8f4e35087b90eeb92a4698c8
Thomas Weber <tweber36@gmail.com>
2020-05-31 23:43:44 +0200

    modify p1-2 after pyler changes

--- solutions/problem_0001.py
+++ solutions/problem_0001.py
@@ -1,9 +1,7 @@
-import unittest
-
 from pyler.pyler import EulerProblem
 
 
-class Problem0001(EulerProblem, unittest.TestCase):
+class Problem0001(EulerProblem):
     """
     If we list all the natural numbers below 10 that are multiples of 3 or 5, we
     get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the
@@ -16,10 +14,11 @@ class Problem0001(EulerProblem, unittest.TestCase):
     real_input = 999
     real_output = 233168
 
-    def solver(self, input_val):
+    @staticmethod
+    def solver(input_val):
         """
         Sum of numbers divisible by 3 (=3+6+9+..+999=3*(1+2+3+..+333)) +
-        sum of numbers divisble by 5 (=5+10+..+995=5*(1+2+..+199)) -
+        sum of numbers divisible by 5 (=5+10+..+995=5*(1+2+..+199)) -
         sum of numbers divisible by 15 (=15+30+..+990=15*(1+2+..+66))
         """
         return (
@@ -28,10 +27,12 @@ class Problem0001(EulerProblem, unittest.TestCase):
             - 15 * (input_val // 15 * (input_val // 15 + 1) // 2)
         )
 
-    def solver2(self, input_val):
+    @staticmethod
+    def solver2(input_val):
         """ One-liner but more than 100 times slower"""
         return sum(x for x in range(input_val + 1) if x % 3 == 0 or x % 5 == 0)
 
 
 if __name__ == "__main__":
+    import unittest
     unittest.main()
